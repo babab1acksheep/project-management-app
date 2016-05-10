78dd3132515501e23a9d30323ddf1165ea2a03e8
Aravinth <aravinth.chandramouli@gmail.com>
2016-05-10 15:05:43 +0530

    MEAN project initial commit

---
+++ public/js/services/UserService.js
@@ -0,0 +1,59 @@
+angular.module("UserService",[]).factory('UserService', ['$http', '$q', function($http, $q){
+
+	return {
+		
+/*			fetchAllUsers: function(baseUrl) {
+					return $http.get(baseUrl+'user/')
+							.then(
+									function(response){
+										return response.data;
+									}, 
+									function(errResponse){
+										console.error('Error while fetching users');
+										return $q.reject(errResponse);
+									}
+							);
+			},*/
+		    
+		    createUser: function(user,baseUrl){
+					return $http.post(baseUrl+'user/add', user)
+							.then(
+									function(response){
+										return response.data;
+									}, 
+									function(errResponse){
+										console.error('Error while creating user');
+										return $q.reject(errResponse);
+									}
+							);
+		    }
+		    
+/*		    updateUser: function(user, id,baseUrl){
+					return $http.put(baseUrl+'user/'+id, user)
+							.then(
+									function(response){
+										return response.data;
+									}, 
+									function(errResponse){
+										console.error('Error while updating user');
+										return $q.reject(errResponse);
+									}
+							);
+			},
+		    
+			deleteUser: function(id,baseUrl){
+					return $http.post(baseUrl+'user/'+id)
+							.then(
+									function(response){
+										return response.data;
+									}, 
+									function(errResponse){
+										console.error('Error while deleting user');
+										return $q.reject(errResponse);
+									}
+							);
+			}*/
+		
+	};
+
+}]);
